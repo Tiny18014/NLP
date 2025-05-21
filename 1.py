f2413fc5f5d472b73f0e8312e18785d65d635c3b
Shubham Mahanti <128962677+Tiny18014@users.noreply.github.com>
2025-05-21 22:26:00 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -5,6 +5,36 @@ from nltk.stem import PorterStemmer, WordNetLemmatizer
 
 for res in ['punkt', 'stopwords', 'wordnet', 'punkt_tab']:
     nltk.download(res)
+print(""" import nltk
+from nltk.tokenize import word_tokenize, sent_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer, WordNetLemmatizer
+
+for res in ['punkt', 'stopwords', 'wordnet', 'punkt_tab']:
+    nltk.download(res)
+    text1 = "Mumbai is the financial capital of India. It is known for Bollywood and its street food. I love going there"
+text2 = "The Taj Mahal is located in Agra. It was built by Emperor Shah Jahan in memory of his wife."
+
+sentences1 = sent_tokenize(text1)
+sentences2 = sent_tokenize(text2)
+print(sentences1)
+
+
+words1 = word_tokenize(text1)
+words2 = word_tokenize(text2)
+print(words1)
+
+stop_words = set(stopwords.words('english'))
+filtered_words1 = [word for word in words1 if word.lower() not in stop_words]
+print(filtered_words1)
+
+stemmer = PorterStemmer()
+stemmed_words1 = [stemmer.stem(word) for word in filtered_words1]
+print(stemmed_words1)
+
+lemmatizer = WordNetLemmatizer()
+lemmatized_words1 = [lemmatizer.lemmatize(word) for word in filtered_words1]
+print(lemmatized_words1) """)
 
 text1 = "Mumbai is the financial capital of India. It is known for Bollywood and its street food. I love going there"
 text2 = "The Taj Mahal is located in Agra. It was built by Emperor Shah Jahan in memory of his wife."
@@ -29,3 +59,4 @@ print(stemmed_words1)
 lemmatizer = WordNetLemmatizer()
 lemmatized_words1 = [lemmatizer.lemmatize(word) for word in filtered_words1]
 print(lemmatized_words1)
+
